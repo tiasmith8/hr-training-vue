72500d3ed930ee397e201d4cc86aa241b24a34b9
Tia Smith <tsmith@onosys.com>
2022-09-08 15:50:14 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <!-- Normal html element (nav) -->
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img class="logo" src="./assets/hr-icon.png" />
+          HR Training
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <HomePage />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HomePage from './domain/home/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HomePage,
   },
 };
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fiixed;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+main {
+  margin: 0 auto;
+  /* This will center the main content */
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+
+header {
+  background-color: #999;
+  width: 1084px;
+  margin: 0 auto;
+}
+
+ul {
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo {
+  vertical-align: middle;
+  height: 30px;
 }
 </style>
---
+++ src/domain/home/HomePage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="hr-training" src="../../assets/hr-training.jpg" aria-hidden="true" />
+    </div>
+    <div>
+      <a href="">Create your first workout!</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // Name of the component
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+
+.hr-training {
+  height: 300px;
+}
+</style>
